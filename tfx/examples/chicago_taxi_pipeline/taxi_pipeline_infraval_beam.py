72eaedb290de4ceffe31817d4f33cbcb1f9e8b5f
jjong <jjong@google.com>
2021-04-02 08:41:25 -0700

    Make model input in Pusher optional.
    InfraValidator can now produce a model in the InfraBlessing output artifact. In order to let Pusher push both Model and InfraBlessing, we allow using InfraBlessing as a source model if Model input is not given.
    
    PiperOrigin-RevId: 366451093

--- tfx/examples/chicago_taxi_pipeline/taxi_pipeline_infraval_beam.py
+++ tfx/examples/chicago_taxi_pipeline/taxi_pipeline_infraval_beam.py
@@ -1,4 +1,3 @@
-# Lint as: python2, python3
 # Copyright 2019 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,36 +13,24 @@
 # limitations under the License.
 """Chicago taxi example using TFX."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
-from typing import List, Text
+from typing import List
 
 import absl.logging
-import tensorflow_model_analysis as tfma
 
 from tfx.components import CsvExampleGen
-from tfx.components import Evaluator
-from tfx.components import ExampleValidator
 from tfx.components import InfraValidator
 from tfx.components import Pusher
-from tfx.components import ResolverNode
 from tfx.components import SchemaGen
 from tfx.components import StatisticsGen
 from tfx.components import Trainer
 from tfx.components import Transform
-from tfx.dsl.experimental import latest_blessed_model_resolver
 from tfx.orchestration import metadata
 from tfx.orchestration import pipeline
 from tfx.orchestration.beam.beam_dag_runner import BeamDagRunner
 from tfx.proto import infra_validator_pb2
 from tfx.proto import pusher_pb2
 from tfx.proto import trainer_pb2
-from tfx.types import Channel
-from tfx.types.standard_artifacts import Model
-from tfx.types.standard_artifacts import ModelBlessing
 
 _pipeline_name = 'chicago_taxi_infraval_beam'
 
@@ -77,10 +64,16 @@ _beam_pipeline_args = [
 
 
 # TODO(b/137289334): rename this as simple after DAG visualization is done.
-def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
-                     module_file: Text, serving_model_dir: Text,
-                     metadata_path: Text,
-                     beam_pipeline_args: List[Text]) -> pipeline.Pipeline:
+def _create_pipeline(
+    pipeline_name: str,
+    pipeline_root: str,
+    data_root: str,
+    module_file: str,
+    serving_model_dir: str,
+    metadata_path: str,
+    beam_pipeline_args: List[str],
+    make_warmup: bool,
+) -> pipeline.Pipeline:
   """Implements the chicago taxi pipeline with TFX."""
   # Brings data into the pipeline or otherwise joins/converts training data.
   example_gen = CsvExampleGen(input_base=data_root)
@@ -92,11 +85,6 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
   schema_gen = SchemaGen(
       statistics=statistics_gen.outputs['statistics'],
       infer_feature_shape=False)
-
-  # Performs anomaly detection based on statistics and data schema.
-  example_validator = ExampleValidator(
-      statistics=statistics_gen.outputs['statistics'],
-      schema=schema_gen.outputs['schema'])
 
   # Performs transformations and feature engineering in training and serving.
   transform = Transform(
@@ -113,41 +101,6 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
       train_args=trainer_pb2.TrainArgs(num_steps=10000),
       eval_args=trainer_pb2.EvalArgs(num_steps=5000))
 
-  # Get the latest blessed model for model validation.
-  model_resolver = ResolverNode(
-      instance_name='latest_blessed_model_resolver',
-      resolver_class=latest_blessed_model_resolver.LatestBlessedModelResolver,
-      model=Channel(type=Model),
-      model_blessing=Channel(type=ModelBlessing))
-
-  # Uses TFMA to compute a evaluation statistics over features of a model and
-  # perform quality validation of a candidate model (compared to a baseline).
-  eval_config = tfma.EvalConfig(
-      model_specs=[tfma.ModelSpec(signature_name='eval')],
-      slicing_specs=[
-          tfma.SlicingSpec(),
-          tfma.SlicingSpec(feature_keys=['trip_start_hour'])
-      ],
-      metrics_specs=[
-          tfma.MetricsSpec(
-              thresholds={
-                  'accuracy':
-                      tfma.config.MetricThreshold(
-                          value_threshold=tfma.GenericValueThreshold(
-                              lower_bound={'value': 0.6}),
-                          # Change threshold will be ignored if there is no
-                          # baseline model resolved from MLMD (first run).
-                          change_threshold=tfma.GenericChangeThreshold(
-                              direction=tfma.MetricDirection.HIGHER_IS_BETTER,
-                              absolute={'value': -1e-10}))
-              })
-      ])
-  evaluator = Evaluator(
-      examples=example_gen.outputs['examples'],
-      model=trainer.outputs['model'],
-      baseline_model=model_resolver.outputs['model'],
-      eval_config=eval_config)
-
   # Performs infra validation of a candidate model to prevent unservable model
   # from being pushed.
   infra_validator = InfraValidator(
@@ -158,14 +111,13 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
               tags=['latest']),
           local_docker=infra_validator_pb2.LocalDockerConfig()),
       request_spec=infra_validator_pb2.RequestSpec(
-          tensorflow_serving=infra_validator_pb2.TensorFlowServingRequestSpec())
-  )
+          tensorflow_serving=infra_validator_pb2.TensorFlowServingRequestSpec(),
+          make_warmup=make_warmup))
 
   # Checks whether the model passed the validation steps and pushes the model
   # to a file destination if check passed.
   pusher = Pusher(
-      model=trainer.outputs['model'],
-      model_blessing=evaluator.outputs['blessing'],
+      model=None if make_warmup else trainer.outputs['model'],
       infra_blessing=infra_validator.outputs['blessing'],
       push_destination=pusher_pb2.PushDestination(
           filesystem=pusher_pb2.PushDestination.Filesystem(
@@ -178,11 +130,8 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
           example_gen,
           statistics_gen,
           schema_gen,
-          example_validator,
           transform,
           trainer,
-          model_resolver,
-          evaluator,
           infra_validator,
           pusher,
       ],
@@ -205,4 +154,5 @@ if __name__ == '__main__':
           module_file=_module_file,
           serving_model_dir=_serving_model_dir,
           metadata_path=_metadata_path,
-          beam_pipeline_args=_beam_pipeline_args))
+          beam_pipeline_args=_beam_pipeline_args,
+          make_warmup=False))
--- tfx/examples/chicago_taxi_pipeline/taxi_pipeline_infraval_beam_e2e_test.py
+++ tfx/examples/chicago_taxi_pipeline/taxi_pipeline_infraval_beam_e2e_test.py
@@ -14,16 +14,22 @@
 """E2E Tests for tfx.examples.chicago_taxi_pipeline.taxi_pipeline_infraval_beam."""
 
 import os
-from typing import Text
 import unittest
 
 from absl.testing import parameterized
 import tensorflow as tf
-from tfx.dsl.components.base import base_driver
 from tfx.dsl.io import fileio
 from tfx.examples.chicago_taxi_pipeline import taxi_pipeline_infraval_beam
 from tfx.orchestration import metadata
 from tfx.orchestration.beam.beam_dag_runner import BeamDagRunner
+from tfx.utils import path_utils
+
+from ml_metadata.proto import metadata_store_pb2
+
+_OUTPUT_EVENT_TYPES = [
+    metadata_store_pb2.Event.OUTPUT,
+    metadata_store_pb2.Event.DECLARED_OUTPUT,
+]
 
 
 @unittest.skipIf(tf.__version__ < '2',
@@ -46,7 +52,10 @@ class TaxiPipelineInfravalBeamEndToEndTest(
     self._metadata_path = os.path.join(self._test_dir, 'tfx', 'metadata',
                                        self._pipeline_name, 'metadata.db')
 
-  def assertExecutedOnce(self, component: Text) -> None:
+  def assertFileExists(self, path):
+    self.assertTrue(fileio.exists(path), f'{path} does not exist.')
+
+  def assertExecutedOnce(self, component: str) -> None:
     """Check the component is executed exactly once."""
     component_path = os.path.join(self._pipeline_root, component)
     self.assertTrue(fileio.exists(component_path))
@@ -65,22 +74,31 @@ class TaxiPipelineInfravalBeamEndToEndTest(
       execution = fileio.listdir(os.path.join(component_path, output))
       self.assertLen(execution, 1)
 
-  def assertInfraValidatorPassed(self) -> None:
-    infra_validator_path = os.path.join(self._pipeline_root, 'InfraValidator')
-    blessing_path = os.path.join(self._pipeline_root, 'InfraValidator',
-                                 'blessing')
-    executions = fileio.listdir(blessing_path)
-    self.assertGreaterEqual(len(executions), 1)
-    for exec_id in executions:
-      blessing_uri = base_driver._generate_output_uri(  # pylint: disable=protected-access
-          infra_validator_path, 'blessing', exec_id)
-      blessed = os.path.join(blessing_uri, 'INFRA_BLESSED')
-      self.assertTrue(fileio.exists(blessed))
+  def _get_latest_output_artifact(self, component_name, output_key):
+    metadata_config = metadata.sqlite_metadata_connection_config(
+        self._metadata_path)
+    with metadata.Metadata(metadata_config) as m:
+      [exec_type_name] = [
+          exec_type.name for exec_type in m.store.get_execution_types()
+          if component_name in exec_type.name]
+      executions = m.store.get_executions_by_type(exec_type_name)
+      events = m.store.get_events_by_execution_ids([e.id for e in executions])
+      output_artifact_ids = [event.artifact_id for event in events
+                             if event.type in _OUTPUT_EVENT_TYPES]
+      output_artifacts = m.store.get_artifacts_by_id(output_artifact_ids)
+      self.assertNotEmpty(output_artifacts)
+      return max(output_artifacts, key=lambda a: a.create_time_since_epoch)
+
+  def assertInfraValidatorPassed(self):
+    blessing = self._get_latest_output_artifact('InfraValidator', 'blessing')
+    self.assertFileExists(os.path.join(blessing.uri, 'INFRA_BLESSED'))
+
+  def assertPushedModelHasWarmup(self):
+    pushed_model = self._get_latest_output_artifact('Pusher', 'pushed_model')
+    self.assertFileExists(path_utils.warmup_file_path(pushed_model.uri))
 
   def assertPipelineExecution(self) -> None:
     self.assertExecutedOnce('CsvExampleGen')
-    self.assertExecutedOnce('Evaluator')
-    self.assertExecutedOnce('ExampleValidator')
     self.assertExecutedOnce('InfraValidator')
     self.assertExecutedOnce('Pusher')
     self.assertExecutedOnce('SchemaGen')
@@ -88,8 +106,11 @@ class TaxiPipelineInfravalBeamEndToEndTest(
     self.assertExecutedOnce('Trainer')
     self.assertExecutedOnce('Transform')
 
-  def testTaxiPipelineBeam(self):
-    num_components = 10
+  @parameterized.named_parameters(
+      ('_withoutWarmup', False),
+      ('_withWarmup', True))
+  def testInfraValidatorPipeline(self, make_warmup):
+    num_components = 7
 
     BeamDagRunner().run(
         taxi_pipeline_infraval_beam._create_pipeline(
@@ -99,7 +120,8 @@ class TaxiPipelineInfravalBeamEndToEndTest(
             serving_model_dir=self._serving_model_dir,
             pipeline_root=self._pipeline_root,
             metadata_path=self._metadata_path,
-            beam_pipeline_args=[]))
+            beam_pipeline_args=[],
+            make_warmup=make_warmup))
 
     self.assertTrue(fileio.exists(self._serving_model_dir))
     self.assertTrue(fileio.exists(self._metadata_path))
@@ -113,44 +135,8 @@ class TaxiPipelineInfravalBeamEndToEndTest(
 
     self.assertPipelineExecution()
     self.assertInfraValidatorPassed()
-
-    # Runs pipeline the second time.
-    BeamDagRunner().run(
-        taxi_pipeline_infraval_beam._create_pipeline(
-            pipeline_name=self._pipeline_name,
-            data_root=self._data_root,
-            module_file=self._module_file,
-            serving_model_dir=self._serving_model_dir,
-            pipeline_root=self._pipeline_root,
-            metadata_path=self._metadata_path,
-            beam_pipeline_args=[]))
-
-    # All executions but Evaluator and Pusher are cached.
-    # Note that Resolver will always execute.
-    with metadata.Metadata(metadata_config) as m:
-      # Artifact count is increased by 3 caused by Evaluator and Pusher.
-      self.assertLen(m.store.get_artifacts(), artifact_count + 3)
-      artifact_count = len(m.store.get_artifacts())
-      # 10 more cached executions.
-      self.assertLen(m.store.get_executions(), num_components * 2)
-
-    # Runs pipeline the third time.
-    BeamDagRunner().run(
-        taxi_pipeline_infraval_beam._create_pipeline(
-            pipeline_name=self._pipeline_name,
-            data_root=self._data_root,
-            module_file=self._module_file,
-            serving_model_dir=self._serving_model_dir,
-            pipeline_root=self._pipeline_root,
-            metadata_path=self._metadata_path,
-            beam_pipeline_args=[]))
-
-    # Asserts cache execution.
-    with metadata.Metadata(metadata_config) as m:
-      # Artifact count is unchanged.
-      self.assertLen(m.store.get_artifacts(), artifact_count)
-      # 10 more cached executions.
-      self.assertLen(m.store.get_executions(), num_components * 3)
+    if make_warmup:
+      self.assertPushedModelHasWarmup()
 
 
 if __name__ == '__main__':
